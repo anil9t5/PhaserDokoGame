5513e820ac4579d8f912f4110806dfc0e1a65bb8
Anil Hitang <anilhitang@MacBook-Pro.local>
2025-03-18 16:02:22 -0300

    Basic working game

--- main.js
+++ main.js
@@ -1,38 +1,151 @@
-import "./style.css";
 import Phaser from "phaser";
 
-const sizes = {
-  width: 500,
-  height: 500,
-};
+const sizes = { width: 500, height: 500 };
 
-const speedDown = 300
+const speedDown = 300;
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
+    this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
+    this.target;
+    this.points = 0;
+    this.textScore;
+    this.textTime;
+    this.timedEvent;
+    this.remainingTime;
+    this.coinMusic;
+    this.bgMusic;
+    this.emitter;
+    this.particles;
   }
 
+  //Preload function logic
   preload() {
+    this.load.image("bg", "public/assets/bg.png");
+    this.load.image("basket", "public/assets/basket.png");
+    this.load.image("apple", "public/assets/apple.png");
+    this.load.image("money", "public/assets/money.png");
+    this.load.audio("coin", "public/assets/coin.mp3");
+    this.load.audio("bgMusic", "public/assets/bgMusic.mp3");
   }
 
+  //Create function logic
   create() {
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+
+    this.player = this.physics.add
+      .image(0, sizes.height - 100, "basket")
+      .setOrigin(0, 0);
+    this.player.setImmovable(true);
+    this.player.body.allowGravity = false;
+    this.player.setCollideWorldBounds(true);
+    // this.player.setSize(80, 15).setOffset(10, 70);
+    this.player
+      .setSize(
+        this.player.width - this.player.width / 4,
+        this.player.height / 6
+      )
+      .setOffset(
+        this.player.width / 8,
+        this.player.height - this.player.height / 8
+      );
+
+    this.target = this.physics.add.image(0, 0, "apple").setOrigin(0, 0);
+    this.target.setMaxVelocity(0, speedDown);
+    this.physics.add.overlap(
+      this.target,
+      this.player,
+      this.targetHit,
+      null,
+      this //context
+    );
+
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    this.textScore = this.add.text(sizes.width - 120, 10, "Score: 0", {
+      font: "25px Arial",
+      fill: "#000000",
+    });
+
+    this.textTime = this.add.text(10, 10, "Remaining Time: 00", {
+      font: "25px Arial",
+      fill: "#000000",
+    });
+
+    this.timedEvent = this.time.delayedCall(3000, this.gameOver, [], this);
+
+    //Emitter...coin
+    const emitter = this.add.particles(100, 300, "money", {
+      frame: "red",
+      angle: { min: -30, max: 30 },
+      speed: 150,
+      scale: 0.04,
+    });
+    //Add Music
+    this.coinMusic = this.sound.add("coin");
+    this.bgMusic = this.sound.add("bgMusic");
+    this.bgMusic.play();
+    this.bgMusic.stop();
   }
 
+  //Update function logic
   update() {
+    const { left, right } = this.cursor;
+
+    this.targetFallsOffScreen();
+
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+
+    this.remainingTime = this.timedEvent.getRemainingSeconds();
+    this.textTime.setText(
+      `Remaining Time: ${Math.round(this.remainingTime).toString()}`
+    );
   }
 
+  getRandomX() {
+    return Math.floor(Math.random() * 480);
+  }
+
+  targetFallsOffScreen() {
+    if (this.target.y >= sizes.height) {
+      this.target.setY(0);
+      this.target.setX(this.getRandomX());
+    }
+  }
+
+  targetHit() {
+    this.coinMusic.play();
+    this.target.setY(0);
+    this.target.setX(this.getRandomX());
+    this.emitter.start();
+    this.points++;
+    this.textScore.setText(`Score: ${this.points}`);
+  }
+
+  gameOver() {
+    console.log();
+  }
 }
 
 const config = {
-  type: Phaser.WEBGL,
+  type: Phaser.AUTO,
   width: sizes.width,
   height: sizes.height,
-  canvas: gameCanvas,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: speedDown },
+      gravity: {
+        y: speedDown,
+      },
       debug: true,
     },
   },
